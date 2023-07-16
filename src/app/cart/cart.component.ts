941b301461015d311c979867b3ccca83af1ec4f5
Santosh <santoshbehara.sb@gmail.com>
2023-07-16 21:33:26 +0530

    Cart checkout functionality done at UI

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../_services/product.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cart',
@@ -13,7 +14,7 @@ export class CartComponent implements OnInit{
   displayedColumns: string[] = ['Product Name', 'description', 'Product Actual Price', 'Product Discounted Price'];
   
 
-  constructor(private productService: ProductService) {}
+  constructor(private productService: ProductService, private router: Router) {}
 
   ngOnInit(): void {
     // throw new Error('Method not implemented.');
@@ -30,5 +31,20 @@ export class CartComponent implements OnInit{
       }
     );
   }
+
+  checkout(){
+    this.router.navigate(['/buyProduct', {
+      isSingleProductCheckout: false, id: 0
+    }]);
+
+    // this.productService.getProductDetails(false, 0).subscribe(
+    //   (resp) => {
+    //     console.log(resp);
+    //   }, (err) => {
+    //     console.log(err);
+    //   }
+    // );
+  }
+
 
 }
